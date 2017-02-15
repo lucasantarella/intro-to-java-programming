f71912b73c94e6713b45b1ed22102e63e78658e5
Luca Santarella <luca.santarella@gmail.com>
2017-02-15 12:36:06 -0500

    Fixed spacing

--- Chapter 2/src/com/lucasantarella/introtoprogramming/lab/Shapes2.java
+++ Chapter 2/src/com/lucasantarella/introtoprogramming/lab/Shapes2.java
@@ -3,7 +3,7 @@ package com.lucasantarella.introtoprogramming.lab;
 public class Shapes2 {
 	
 	public static void main(String[] args){
-		System.out.println("*********	    ***  	    *  	        *              *		");
+		System.out.println("*********	    ***  	    *  	        *              *        ");
 		System.out.println("*       *	   *   * 	   *** 	       * *            * *       ");
 		System.out.println("*       *	  *     *	  *****	      *   *          *   *      ");
 		System.out.println("*       *	  *     *	    *  	     *     *        *     *     ");
